ed1f4b557f5630545fdf9030c8bbb2c63665999d
Thomas <thomi.reynoso@hotmail.com>
2019-11-01 23:43:11 -0300

    Creación de un nuevo Component
    Se configura el decorador Component, y se le añade las prop Selector y Template, donde, ademàs se ve interpolación de variables. Este se muestra en pantalla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './components/mi-componente/mi-componente.component'; //cargo la clase/comp creado
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente
   ],
-  imports: [
+  imports: [ 
     BrowserModule
   ],
   providers: [],
---
+++ src/app/components/mi-componente/mi-componente.component.ts
@@ -0,0 +1,29 @@
+import  { Component } from '@angular/core'; //importo el comp ppal
+
+@Component({ //OBLIGATORIAMENTE lleva prop selector y template
+	selector: 'mi-componente', //es la etiqueta de la vista
+	template: ` 
+		<h1>{{titulo}} </h1>
+		<h2> {{comentario}} </h2>
+		<p> {{year}} </p>
+		
+	` //entre estas comillas puedo poner el codigo html que quiera
+
+	}) //es un JSON y es imp NO cerrarlo con ;
+
+export class MiComponente{
+
+	public titulo: string;
+	public comentario: string;
+	public year: number;
+	//Array<any> arrays de tipo any
+
+	constructor(){
+		this.titulo = "Hola Mundo, soy MI COMPONENTE";
+		this.comentario = " Este es el primer componente creado";
+		this.year = 2019;
+		
+		console.log("Componente mi-componente cargado!!");
+		console.log(this.titulo, this.comentario, this.year);
+	}
+}
